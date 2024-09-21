50afc5b180445e58d80c479ddbb2bfa965f96f4c
Christine1302 <107089652+Christine1302@users.noreply.github.com>
2024-09-21 15:35:01 -0400

    Update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,16 @@
+// module.exports = {
+//   style: {
+//     postcss: {
+//       plugins: [
+//         require('tailwindcss'),
+//         require('autoprefixer'),
+//       ],
+//     },
+//   },
+// }
+
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   style: {
     postcss: {
@@ -7,4 +20,38 @@ module.exports = {
       ],
     },
   },
-}
+  webpack: {
+    configure: (webpackConfig) => {
+      // Babel loader for modern JavaScript (to fix the minification issue)
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(@remix-run\/router))/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      });
+
+      // Terser for minification (optional, in case react-scripts minification fails)
+      if (webpackConfig.optimization && webpackConfig.optimization.minimizer) {
+        webpackConfig.optimization.minimizer = [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: 2020, // Adjust to the ECMAScript version you're targeting
+              parse: {},
+              compress: {},
+              mangle: true,
+              output: {
+                comments: false,
+              },
+            },
+          }),
+        ];
+      }
+
+      return webpackConfig;
+    },
+  },
+};
