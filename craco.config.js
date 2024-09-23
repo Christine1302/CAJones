5ebb922c6a9fd77f29e8510fe2b0736d612ffe04
Christine1302 <107089652+Christine1302@users.noreply.github.com>
2024-09-23 17:47:30 -0400

    Add files via upload

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,12 @@ module.exports = {
         require('tailwindcss'),
         require('autoprefixer'),
       ],
-      minify: false,
     },
   },
-}
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.optimization.minimize = false;
+      return webpackConfig;
+    },
+  },
+};
